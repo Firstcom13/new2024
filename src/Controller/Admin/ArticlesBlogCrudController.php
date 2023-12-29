64c1048acb9e520c9488c0b3223c9a233e4ee40a
support@firstcom.fr <Hamid@FIRSTCOM.LOCAL>
2023-12-29 12:07:32 +0100

    Blog - 9 - 29/12/2023

--- src/Controller/Admin/ArticlesBlogCrudController.php
+++ src/Controller/Admin/ArticlesBlogCrudController.php
@@ -4,10 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\ArticlesBlog;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CodeEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
@@ -42,21 +44,13 @@ class ArticlesBlogCrudController extends AbstractCrudController
             ];
         } else {
             return [
+                FormField::addColumn('col-12 col-lg-8'),
                 TextEditorField::new('titre'),
                 TextEditorField::new('description_courte', 'Description Courte'),
-                TextEditorField::new('contenu')
-                    ->setTrixEditorConfig([
-                        'blockAttributes' => [
-                            'default' => ['tagName' => 'p'],
-                            'heading1' => ['tagName' => 'h1'],
-                            'heading2' => ['tagName' => 'h2'],
-                            'heading3' => ['tagName' => 'h3'],
-                            // ...ajoutez d'autres niveaux de titres si nécessaire
-                        ],
-                        'css' => [
-                            'attachment' => 'admin_file_field_attachment',
-                        ],
-                    ]),
+                CodeEditorField::new('contenu'),
+
+
+                FormField::addColumn('col-12 col-lg-4'),
                 ImageField::new('img_s', 'Petite Image')
                     ->setBasePath('uploads/images')
                     ->setUploadDir('public/uploads/images'),
@@ -65,7 +59,6 @@ class ArticlesBlogCrudController extends AbstractCrudController
                     ->setUploadDir('public/uploads/images'),
                 DateTimeField::new('date_creation', 'Date de Publication')
                     ->setFormat('dd/MM/YYYY HH:mm'),
-
                 AssociationField::new('categorie', 'Catégories')
                 ->setFormTypeOptions([
                     'by_reference' => false, // Important pour les relations ManyToMany

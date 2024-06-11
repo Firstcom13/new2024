4c325eef6cbc82245e8b6bf2a5c9d2b02abe5ab8
support@firstcom.fr <Hamid@FIRSTCOM.LOCAL>
2024-06-11 16:54:18 +0200

    Refonte contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -24,24 +24,24 @@ class ContactController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted())  {
+        if ($form->isSubmitted() && $form->isValid()) {
 
             // Obtenir la réponse du captcha
-            $captchaResponse = $request->request->get('g-recaptcha-response');
+            $captchaResponse = $request->request->get('h-captcha-response');
 
-            // Préparer la requête à l'API de vérification de reCAPTCHA
-            $recaptchaUrl = 'https://www.google.com/recaptcha/api/siteverify';
-            $recaptchaSecret = $this->getParameter('google_recaptcha_secret');
+            // Préparer la requête à l'API de vérification de hCaptcha
+            $recaptchaUrl = 'https://hcaptcha.com/siteverify';
+            $recaptchaSecret = $this->getParameter('hcaptcha_secret_key');
 
-            $recaptchaUrl .= '?secret=' . $recaptchaSecret . '&response=' . $captchaResponse;
+            $data = [
+                'secret' => $recaptchaSecret,
+                'response' => $captchaResponse,
+                'remoteip' => $request->getClientIp()
+            ];
 
             // Exécuter la requête
-            $verifyResponse = file_get_contents($recaptchaUrl);
-
-            // Décoder la réponse
+            $verifyResponse = file_get_contents($recaptchaUrl . '?' . http_build_query($data));
             $responseData = json_decode($verifyResponse);
-
-            // error_log(print_r($responseData, true));
 
             // Vérifier que le captcha a été validé
             if (!$responseData->success) {
@@ -50,62 +50,65 @@ class ContactController extends AbstractController
                 return $this->redirectToRoute('app_contact');
             }
 
-            if ($form->isValid()) {
+            $data = $form->getData();
 
-                $data = $form->getData();
+            $email = $data->getEmail();
+            $message = $data->getMessage();
+            $data->setDateCreation();
 
-                $email = $data->getEmail(); // Access the email property using the getEmail() method
-                $message = $data->getMessage(); // Access the message property using the getMessage() method
-                $data->setDateCreation();
-            
-                $entityManager->persist($data);         
-                $entityManager->flush();
-                
+            $entityManager->persist($data);         
+            $entityManager->flush();
 
-                $emailObject = (new Email())
-                    ->from('contact@firstcom.fr')
-                    ->to($email)
-                    ->subject('FIRSTCOM - Votre demande de contact...')
-                    ->text($message)
-                    ->html('<p>Bonjour</p>
-                            <p>Nous avons bien reçu votre demande de contact.</p>
-                            <p>Nous ne manquerons pas de revenir vers vous prochainement.</p>
-                            <p>À bientôt.</p>
-                            <p>Firstcom.</p>');
+            $emailObject = (new Email())
+                ->from('contact@firstcom.fr')
+                ->to($email)
+                ->subject('FIRSTCOM - Votre demande de contact...')
+                ->text($message)
+                ->html('<p>Bonjour</p>
+                        <p>Nous avons bien reçu votre demande de contact.</p>
+                        <p>Nous ne manquerons pas de revenir vers vous prochainement.</p>
+                        <p>À bientôt.</p>
+                        <p>Firstcom.</p>');
 
-                try {
-                    $mailer->send($emailObject);
-                } catch (\Symfony\Component\Mailer\Exception\TransportException $e) {
-                    // Vous pouvez ici logger l'erreur, afficher un message, etc.
-                    error_log($e->getMessage());
-                }
-                            
-
-                $managerEmail = "fabrice@firstcom.fr"; 
-
-                $managerEmailObject = (new Email())
-                    ->from('contact@firstcom.fr')
-                    ->to($managerEmail)
-                    ->subject('Une nouvelle demande de contact a été reçue')
-                    ->text('Une nouvelle demande de contact a été reçue de ' . $email . '. Le message était : ' . $message)
-                    ->html('<p>Bonjour,</p>
-                                <p>Une nouvelle demande de contact a été reçue de ' . $email . '.</p>
-                                <p>Le message était : </p>
-                                <p>' . $message . '</p>');
-
-                try {
-                    $mailer->send($managerEmailObject);
-                } catch (\Symfony\Component\Mailer\Exception\TransportException $e) {
-                    // Vous pouvez ici logger l'erreur, afficher un message, etc.
-                    error_log($e->getMessage());
-                }
-                        
+            try {
+                $mailer->send($emailObject);
+            } catch (\Symfony\Component\Mailer\Exception\TransportException $e) {
+                error_log($e->getMessage());
             }
+
+            $managerEmail = "hamid@firstcom.fr"; 
+
+            $managerEmailObject = (new Email())
+                ->from('contact@firstcom.fr')
+                ->to($managerEmail)
+                ->subject('Une nouvelle demande de contact a été reçue')
+                ->text('Une nouvelle demande de contact a été reçue de ' . $email . '. Le message était : ' . $message)
+                ->html('<p>Bonjour,</p>
+                            <p>Une nouvelle demande de contact a été reçue de ' . $email . '.</p>
+                            <p>Le message était : </p>
+                            <p>' . $message . '</p>');
+
+            try {
+                $mailer->send($managerEmailObject);
+            } catch (\Symfony\Component\Mailer\Exception\TransportException $e) {
+                error_log($e->getMessage());
+            }
+
+            return $this->redirectToRoute('app_contact_merci'); // Redirection vers une page de succès
         }
 
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
-            'formulaire' => $form->createView(), // Use createView() to render the form
+            'formulaire' => $form->createView(),
+            'hcaptcha_site_key' => $this->getParameter('hcaptcha_site_key'),
         ]);
     }
+
+    #[Route('/contact/merci', name: 'app_contact_merci')]
+    public function success(): Response {
+        return $this->render('contact/merci.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+
 }

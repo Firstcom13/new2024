b9857e3f6073ac9c70d3044ad9c86fc5541417e2
Firstcom13 <110915881+Firstcom13@users.noreply.github.com>
2025-12-16 11:17:22 +0100

    Disable maintenance mode in index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 // ========================================
 // MODE MAINTENANCE TEMPORAIRE
 // ========================================
-$maintenance = true;
+$maintenance = false;
 
 // IP autorisées (accès au site même en maintenance)
 $allowedIps = [
